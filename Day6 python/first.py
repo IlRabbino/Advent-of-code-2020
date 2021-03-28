38d343bf0ce817344034e9d16c57e9a370ef070b
Paolo <paolorabbito@gmail.com>
2021-03-28 15:51:42 +0200

    Day6

---
+++ Day6 python/first.py
@@ -0,0 +1,23 @@
+def answer(answers):
+    total = []
+    for answer in answers:
+        for char in answer:
+            if char in total:
+                print(char)
+            else:
+                total.append(char)
+    return len(total)
+
+str = open("input.txt", "r").read()
+groups = str.split("\n\n")
+
+tot = 0
+
+for group in groups:
+    answers = group.split('\n')
+    tot += answer(answers)
+
+print(tot)
+
+
+
