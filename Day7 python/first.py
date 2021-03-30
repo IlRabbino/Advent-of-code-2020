36dd3dfb40d5b6fb8c34a2df100b3e35ab4fe151
Paolo <paolorabbito@gmail.com>
2021-03-30 02:35:20 +0200

    Day6

---
+++ Day7 python/first.py
@@ -0,0 +1,37 @@
+#i've needed online help to complete this one
+
+def thereIsBag(bags, baggy):
+    queue = []
+    container = []
+    queue.append(baggy)
+
+    while len(queue) != 0:
+        tmp = queue.pop(0)
+
+        for bag in bags:
+            if bag not in container:
+                if tmp in bags[bag]:
+                    queue.append(bag)
+                    container.append(bag)
+    
+    return container
+ 
+ 
+ 
+input = open("input.txt", "r").read()
+rules = input.split("\n")
+bags = {}
+
+
+for rule in rules:
+    outer, inner = rule.split("contain")
+    outer = outer.replace(" bags", "")
+    bags[outer] = inner
+
+myResult = list(dict.fromkeys(thereIsBag(bags, "shiny gold")))
+print(len(myResult))
+
+
+
+
+
